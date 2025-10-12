0d6f4f770240eb571101926432e857c1c03a5413
Samyrai47 <sivolobov.ilya2@gmail.com>
2025-10-12 15:12:06 +0300

    Setup project

---
+++ core/src/main/java/org/mipt/Main.java
@@ -0,0 +1,18 @@
+package org.mipt;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.utils.ScreenUtils;
+
+public class Main extends ApplicationAdapter {
+
+  @Override
+  public void create() {}
+
+  @Override
+  public void render() {
+    ScreenUtils.clear(0.15f, 0.15f, 0.2f, 1f);
+  }
+
+  @Override
+  public void dispose() {}
+}
