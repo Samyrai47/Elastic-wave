9bab0d02233b0d0d7da9be90cda2662263134478
Samyrai47 <sivolobov.ilya2@gmail.com>
2025-10-15 00:32:47 +0300

    Added RT4 for solving differential equation; methods for vector calculations; function for giving velocity to first weight; work with fixed step (time); new javaDoc and fixed existed; Did some cleaning;

--- core/src/main/java/org/mipt/Physics.java
+++ core/src/main/java/org/mipt/Physics.java
@@ -1,7 +1,6 @@
 package org.mipt;
 
 import java.util.List;
-import org.mipt.entity.Spring;
 import org.mipt.entity.Weight;
 
 /** Класс для пересчета физики. */
@@ -11,63 +10,160 @@ public class Physics {
   public Physics() {}
 
   /**
-   * Решаем для каждого груза диф. уравнение каждое dt времени,
-   * получаем для каждого груза текущую позицию
+   * Просчитывает физику для модели
+   *
    * @param weights грузы
    * @param deltaTime dt
-   * */
+   */
   public void applyPhysics(List<Weight> weights, float deltaTime) {
-    for (Weight weight : weights) {
-      calcPhysicsForWeight(weight, deltaTime);
+    calcPhysicsForWeights(weights, deltaTime);
+  }
+
+  /**
+   * Просчитывает положения грузов при помощи решения дифференциального уравнения второго порядка
+   * численным методом Рунге-Кутты
+   *
+   * @param weights грузы
+   * @param deltaTime дельта времени
+   */
+  private static void calcPhysicsForWeights(List<Weight> weights, float deltaTime) {
+    float[] state = new float[weights.size() * 2];
+
+    for (int i = 0; i < weights.size(); i++) {
+      state[i] = weights.get(i).getX();
+    }
+
+    for (int i = 0; i < weights.size(); i++) {
+      state[i + weights.size()] = weights.get(i).getVelocityX();
+    }
+
+    state = RT4(state, weights, deltaTime);
+
+    for (int i = 0; i < weights.size(); i++) {
+      weights.get(i).setX(state[i]);
+      weights.get(i).setVelocityX(state[weights.size() + i]);
     }
   }
 
   /**
-   * Решение диф. уравнения для груза
-   * m_i * x_i'' = k_(i - 1) * (x_(i - 1) - x_i + L_(i - 1))
-   * + k_i * (x_(i + 1) - x_i - L_i - width)
-   * k - жесткость пружины
-   * L - начальная длина пружины
-   * width - ширина груза
-   * @param weight груз
-   * @param deltaTime delta по времени
+   * Реализует численный метод Рунге-Кутты 4-го порядка
+   *
+   * @param state текущее положение грузов
+   * @param weights набор весов
+   * @param h шаг
+   * @return обновленные положения грузов (решение дифференциального уравнения)
    */
-  private static void calcPhysicsForWeight(Weight weight, float deltaTime) {
-    // TODO: добавить проверку на коллизии (или тут, или в weight)
-    Spring leftSpring = weight.getLeftSpring();
-    Spring rightSpring = weight.getRightSpring();
-    float xPrev = leftSpring.getLeftX() / Main.PIXELS_PER_METER;
-    float xCurr = weight.getLeftX() / Main.PIXELS_PER_METER;
-    float xNext = rightSpring.getRightX() / Main.PIXELS_PER_METER;
-    float kPrev = leftSpring.getK();
-    float kNext = rightSpring.getK();
-    float leftLength = leftSpring.getLength() / Main.PIXELS_PER_METER;
-    float rightLength = rightSpring.getLength() / Main.PIXELS_PER_METER;
-    float width = weight.getWidth() / Main.PIXELS_PER_METER;
-    float w = (float) Math.sqrt((kNext + kPrev) / weight.getMass());
-    float xLocal =
-        (kPrev * xPrev + kNext * xNext + kPrev * leftLength - kNext * rightLength - kPrev * width)
-            / (kPrev + kNext);
-    float a = xCurr - xLocal;
-    float b = weight.getVelocityX() / w;
-    float newX = (float) (a * Math.cos(w * deltaTime) + b * Math.sin(w * deltaTime) + xLocal);
-    float newVelocityX =
-        (float) (-a * w * Math.sin(w * deltaTime) + b * w * Math.cos(w * deltaTime));
+  private static float[] RT4(float[] state, List<Weight> weights, float h) {
+    float[] k1 = multipleVector(makeDiff(weights, state), h);
+    float[] k2 = multipleVector(makeDiff(weights, addVectors(state, multipleVector(k1, 0.5f))), h);
+    float[] k3 = multipleVector(makeDiff(weights, addVectors(state, multipleVector(k2, 0.5f))), h);
+    float[] k4 = multipleVector(makeDiff(weights, addVectors(state, k3)), h);
 
-    weight.setVelocityX(newVelocityX);
-    weight.setX(newX * Main.PIXELS_PER_METER);
+    float[] newZ =
+        addVectors(
+            addVectors(addVectors(k1, multipleVector(k2, 2.0f)), multipleVector(k3, 2.0f)), k4);
+    state = addVectors(state, multipleVector(newZ, 1.0f / 6.0f));
+
+    return state;
   }
 
   /**
-   * Двигаем первый груз по X
+   * Создает вектор из производных x и z
    *
-   * @param weights груз
-   * @param dx сдвиг
+   * @param weights набор грузов
+   * @param state текущие положения грузов
+   * @return вектор производных
    */
-  public void changeFirstWeightX(List<Weight> weights, float dx) {
-      Weight firstWeight = weights.get(0);
-      if (firstWeight.getRightX() + dx < weights.get(1).getLeftX()) {
-          firstWeight.setX(firstWeight.getX() + dx);
-      }
+  private static float[] makeDiff(List<Weight> weights, float[] state) {
+    int variablesNumber = state.length / 2;
+    float[] res = new float[2 * variablesNumber];
+
+    for (int i = 0; i < variablesNumber; i++) {
+      float zi = state[i + variablesNumber];
+
+      res[i] = zi;
+      res[variablesNumber + i] = diffZ(i, weights, state);
+    }
+    return res;
+  }
+
+  /**
+   * Находит производную от z <=> Находит вторую производную x.
+   *
+   * @param index номер груза
+   * @param weights набор грузов
+   * @param state текущие положения грузов
+   * @return производную z <=> вторую производную x
+   */
+  private static float diffZ(int index, List<Weight> weights, float[] state) {
+    float leftSpringK = weights.get(index).getLeftSpring().getK();
+    float rightSpringK = weights.get(index).getRightSpring().getK();
+    float leftSpringLen = weights.get(index).getLeftSpring().getLength();
+    float rightSpringLen = weights.get(index).getRightSpring().getLength();
+    float weightM = weights.get(index).getMass();
+    float width = weights.get(index).getWidth();
+
+    float currWeightX = state[index];
+
+    float prevRightX;
+    if (index > 0) {
+      float prevWeightX = state[index - 1];
+      float prevWidth = weights.get(index - 1).getWidth();
+      prevRightX = prevWeightX + prevWidth;
+    } else {
+      prevRightX = weights.getFirst().getLeftSpring().getLeftX(); // левая стенка
+    }
+
+    float nextLeftX;
+    if (index < state.length / 2 - 1) {
+      nextLeftX = state[index + 1];
+    } else {
+      nextLeftX = weights.getLast().getRightSpring().getRightX(); // правая стенка
+    }
+
+    return (leftSpringK * (prevRightX - currWeightX + leftSpringLen)
+            + rightSpringK * (nextLeftX - (currWeightX + width) - rightSpringLen))
+        / weightM;
+  }
+
+  /**
+   * Умножает вектор на число
+   *
+   * @param vector вектор
+   * @param multiplier число, на которое нужно умножить вектор
+   * @return результирующий вектор
+   */
+  private static float[] multipleVector(float[] vector, float multiplier) {
+    float[] diffCopy = vector.clone();
+    for (int i = 0; i < vector.length; i++) {
+      diffCopy[i] = vector[i] * multiplier;
+    }
+
+    return diffCopy;
+  }
+
+  /**
+   * Сложение двух векторов
+   *
+   * @param vector1 первый вектор
+   * @param vector2 второй вектор
+   * @return результат сложения
+   */
+  private static float[] addVectors(float[] vector1, float[] vector2) {
+    float[] res = new float[vector1.length];
+    for (int i = 0; i < vector1.length; i++) {
+      res[i] = vector2[i] + vector1[i];
+    }
+    return res;
+  }
+
+  /**
+   * Придает начальное ускорение первому грузу
+   *
+   * @param weights набор грузов
+   * @param velocityX ускорение, которое нужно придать грузу
+   */
+  public void pushFirstWeight(List<Weight> weights, float velocityX) {
+    weights.get(0).setVelocityX(weights.get(0).getVelocityX() + velocityX);
   }
 }
